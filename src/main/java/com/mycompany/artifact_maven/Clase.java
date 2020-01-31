126e57992a7bf651e43a5b35094d6ad7e76a14e2
David <davidgilm123@gmail.com>
2020-01-31 16:24:27 +0100

    Añadir en rama Remota

---
+++ src/main/java/com/mycompany/artifact_maven/Clase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.artifact_maven;
+
+/**
+ *
+ * @author david
+ */
+public class Clase {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int multiplica;
+        multiplica = a * b;
+        System.out.println(" El resultado es:"+multiplica);
+    }
+    
+}
